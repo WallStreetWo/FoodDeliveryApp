0eef53789906937593b3921a6d0907bc2f48bf49
WallStreetWo <olwenhlesithole@gmail.com>
2025-06-16 11:44:52 +0200

    Craeted my ApplicationDbContext and added data to it

--- obj/Debug/net8.0/FoodDeliveryApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FoodDeliveryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodDeliveryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb7e485810301229d07e3025e5954b18d540487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f876ebef0fc6f82a353cf148a1fc12aed7575f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodDeliveryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodDeliveryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
